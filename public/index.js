80090e078f30fb0d606ece48b7761f546650ef11
Christopher Lima <Naruub7@gmail.com>
2022-10-17 11:01:37 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -5,6 +5,8 @@ async function carregarAnimais(){
 
     const lista = document.getElementById('lista-animais')
 
+    lista.innerHTML = ''
+
     animais.forEach(animal => {
         const item = document.createElement('li')
         item.innerText = animal.nome
@@ -17,10 +19,19 @@ function manipularFormulario(){
     const form_animal = document.getElementById('form-animal')
     const input_nome = document.getElementById('nome')
 
-    form_animal.onsubmit = (event) => {
+    form_animal.onsubmit = async (event) => {
         event.preventDefault()
         const nome_animal = input_nome.value
-        alert('Submite chmamado');
+
+        axios.post('http://localhost:8000/animais', {
+            nome: nome_animal,
+            idade: 4,
+            sexo: 'Masculino',
+            cor: 'branco'
+        })
+
+        carregarAnimais()
+        alert('Animal cadastrado')
     }
 }
 
